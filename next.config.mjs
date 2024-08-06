86880c5b72525df3ae7985326964d8fbdb8428fd
KoshalParwan <koshal.parwan@fasst.io>
2024-08-06 18:40:34 +0400

    [next] code splitting and svg webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    webpack(config, { isServer }) {
+        // Performance optimizations
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+          });
+
+        if (!isServer) {
+            config.optimization.splitChunks = {
+                chunks: 'all',
+                minSize: 20000,
+                maxSize: 70000,
+            };
+        }
+
+        return config;
+    },
+};
 
 export default nextConfig;
