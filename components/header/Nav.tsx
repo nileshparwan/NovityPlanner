9c7438856820b98b11e4bbd499cd9ce4e4f6d329
KoshalParwan <koshal.parwan@fasst.io>
2024-08-06 23:14:52 +0400

    [component] update nav url

--- components/header/Nav.tsx
+++ components/header/Nav.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
-import { buttonVariants } from "../ui/button";
+import Link from 'next/link';
+import { buttonVariants } from '../ui/button';
 
 interface INav {
   className: string;
@@ -9,32 +9,47 @@ const Nav: React.FC<INav> = ({ className }) => {
   return (
     <nav aria-label='Global'>
       <ul className={className}>
-        <li className="border-b md:border-0 md:w-fit">
-          <Link href='/' className={`${buttonVariants({ variant: 'link' })}`}>
+        <li className='border-b md:border-0 md:w-fit'>
+          <Link
+            href='/parking'
+            className={`${buttonVariants({ variant: 'link' })}`}
+          >
             Car Parking
           </Link>
         </li>
 
-        <li className="border-b md:border-0 md:w-fit">
-          <Link href='/' className={`${buttonVariants({ variant: 'link' })}`}>
+        <li className='border-b md:border-0 md:w-fit'>
+          <Link
+            href='/events'
+            className={`${buttonVariants({ variant: 'link' })}`}
+          >
             Events
           </Link>
         </li>
 
-        <li className="border-b md:border-0 md:w-fit">
-          <Link href='/' className={`${buttonVariants({ variant: 'link' })}`}>
+        <li className='border-b md:border-0 md:w-fit'>
+          <Link
+            href='/workshops'
+            className={`${buttonVariants({ variant: 'link' })}`}
+          >
             Workshops
           </Link>
         </li>
 
-        <li className="border-b md:border-0 md:w-fit">
-          <Link href='/' className={`${buttonVariants({ variant: 'link' })}`}>
+        <li className='border-b md:border-0 md:w-fit'>
+          <Link
+            href='/photos'
+            className={`${buttonVariants({ variant: 'link' })}`}
+          >
             Photos
           </Link>
         </li>
 
-        <li className="md:w-fit">
-          <Link href='/' className={`${buttonVariants({ variant: 'link' })}`}>
+        <li className='md:w-fit'>
+          <Link
+            href='/calendar'
+            className={`${buttonVariants({ variant: 'link' })}`}
+          >
             Calendar
           </Link>
         </li>
