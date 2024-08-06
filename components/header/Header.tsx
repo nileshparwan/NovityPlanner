282eec3c5bbe329695b0385cd5db00214a68f56a
KoshalParwan <koshal.parwan@fasst.io>
2024-08-06 19:48:44 +0400

    [component] fix browser errors on header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import type React from 'react';
-import { Search, Bell, Loader } from 'lucide-react';
+import { Search, Bell } from 'lucide-react';
 import { type ReactNode, useCallback, useState } from 'react';
 import { Button, buttonVariants } from '../ui/button';
 import Nav from './Nav';
@@ -53,12 +53,12 @@ export const Header: React.FC<IHeader> = ({
               fill='none'
               viewBox='0 0 24 24'
               stroke='currentColor'
-              stroke-width='2'
+              strokeWidth='2'
             >
               <title>something</title>
               <path
-                stroke-linecap='round'
-                stroke-linejoin='round'
+                strokeLinecap='round'
+                strokeLinejoin='round'
                 d='M4 6h16M4 12h16M4 18h16'
               />
             </svg>
